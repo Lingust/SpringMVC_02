46126cef6f6981465790e3c1d616b3f81053fa22
Lingust <Chenxf@Chenxf-PC>
2016-07-19 19:31:13 +0800

    以此为基础开发连接数据库

---
+++ src/main/java/com/chenxf/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.chenxf.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao {
+
+}
---
+++ src/main/java/com/chenxf/service/UserService.java
@@ -0,0 +1,8 @@
+package com.chenxf.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
