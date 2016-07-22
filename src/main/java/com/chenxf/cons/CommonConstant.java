32e50d169828cbbc8fa885e09f14b2dd198e49b4
Lingust <Chenxf@Chenxf-PC>
2016-07-22 10:23:30 +0800

    修改了login流程，加入session存储信息简化登录后操作流程。新增控制层基类存储对session的操作

---
+++ src/main/java/com/chenxf/cons/CommonConstant.java
@@ -0,0 +1,19 @@
+package com.chenxf.cons;
+
+public class CommonConstant {
+	
+	/**
+	 * Seesion中放用户对象的键名称
+	 */
+	public static final String USER_CONTEXT = "USER_CONTEXT";
+	
+	/**
+	 * 登录前的URL放入Session中的此键中
+	 */
+	public static final String LOGIN_TO_URL = "toURL";
+	
+	/**
+	 * 每页的记录数
+	 */
+	public static final int PAGE_SIZE = 3;
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -4,7 +4,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 	<head>
-		<title>登录系统</title>
+		<title>登录系统(注册测试)</title>
 		<script type="text/javascript">
 			function loadXMLDoc(){
 				checkU('tip');
